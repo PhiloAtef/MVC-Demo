f968dbe77d1c213da22ff95e56dfc7123b0833dc
Philo Atef <philoatefdev@gmail.com>
2024-09-17 18:29:34 +0300

    details and edit tabs

--- MVCAssignment/Controllers/DepartmentController.cs
+++ MVCAssignment/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVCAssignment.BLL.Interfaces;
+using MVCAssignment.DAL.Models;
 
 namespace MVCAssignment.PL.Controllers
 {
@@ -10,16 +11,49 @@ namespace MVCAssignment.PL.Controllers
         {
             _repository = departmentRepository;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             var departments = _repository.GetAll();
             return View(departments);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+               var count = _repository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+           var department = _repository.GetById(id.Value);
+            if (department == null)
+            {
+                return NotFound(); 
+            }
+            return View(department);
+        }
+
+        
     }
 }
 
