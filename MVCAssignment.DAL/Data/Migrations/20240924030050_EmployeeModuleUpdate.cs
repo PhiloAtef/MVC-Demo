6e3a3e06ae042495e709fff97debdad4c6e8f838
Philo Atef <philoatefdev@gmail.com>
2024-09-24 06:01:29 +0300

    updating migrations

---
+++ MVCAssignment.DAL/Data/Migrations/20240924030050_EmployeeModuleUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCAssignment.DAL.Data.Migrations
+{
+    public partial class EmployeeModuleUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
